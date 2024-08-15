947bfaa51a1d8f2ede191325a71e88c8dbe7a8a9
d0pii <iagomarques.patos@gmail.com>
2024-08-15 01:00:17 +0000

    Aula01 Estruturas de dados

---
+++ aula01/Pessoa.java
@@ -0,0 +1,34 @@
+public class Pessoa{
+    private String nome;
+    private int idade;
+
+    public Pessoa(){
+        this("",0);
+    }
+    public Pessoa(String nome, int idade){
+            setNome(nome);
+            setIdade(idade);
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString(){
+        //Concatenação de String
+        //return "Pessoa{ Nome:" + getNome() + ", Idade: " + getIdade() + "}";
+
+        //Interpolação de Strings
+        return String.format("Pessoa{ Nome: %s, Idade: %d }",getNome(),getIdade());
+    }
+}
