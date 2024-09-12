556e62566560b71d5da2953ef77743b479777e2b
d0pii <iagomarques.patos@gmail.com>
2024-09-12 01:01:53 +0000

    código

--- aula06ANTIGA/Principal.java
+++ aula06ANTIGA/Principal.java
@@ -1,7 +1,6 @@
 public class Principal{
     public static void main(String[] args){
         ListaDupla<String> lista = new ListaDupla<String>("Alunos");
-
         lista.imprimeLista();
         lista.addInicio("Rafael");
         lista.imprimeLista();
@@ -9,14 +8,6 @@ public class Principal{
         lista.imprimeLista();
         lista.addFinal("Miguel");
         lista.imprimeLista();
-
-        lista.addMeio("Pedro", 1);
-        lista.imprimeLista();
-        
-        lista.removeMeio(2);
-        lista.imprimeLista();
-    
-
         lista.removeInicio();
         lista.imprimeLista();
         lista.removeFinal();
